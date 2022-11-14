fe7de9b10c1cebf896cae6e7c278f969ee7f8eeb
theved123 <vedpgaikwad@gmail.com>
2022-11-14 22:11:12 +0530

    Welcome Controller Added TO Developer Branch

---
+++ src/main/java/com/SpringBoot/controller/WelcomeController.java
@@ -0,0 +1,10 @@
+package com.SpringBoot.controller;
+
+public class WelcomeController {
+
+	public static void main(String[] args) {
+		String s="I'm Developer 1";
+		System.out.println(s);
+	}
+
+}
